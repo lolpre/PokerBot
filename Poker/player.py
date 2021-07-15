0a9ecd9f8f481ffb0202f8df5be98b176c33c009
carrir2 <72038264+carrir2@users.noreply.github.com>
2021-07-15 19:16:14 -0400

    Update player.py

---
+++ Poker/player.py
@@ -0,0 +1,11 @@
+class Card:
+	def __init__(self, id, balance):
+		self.id = id
+		self.balance = balance
+
+	def getBalance(self):
+		return self.id
+	
+	def setBalance(self, value):
+		self.id = value
+
