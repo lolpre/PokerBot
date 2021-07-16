c62e00837184845784fb8cec20d91ac93e1f3a24
trollface11 <85368850+trollface11@users.noreply.github.com>
2021-07-16 19:06:24 -0400

    Update leaderboard.py

--- Poker/leaderboard.py
+++ Poker/leaderboard.py
@@ -1,18 +1,20 @@
+from player import Player
 class Leaderboard:
     
     def __init__(self):
         self.players=[]
-        self.playerdata={}
         
-    async def printLeaderboard(self, ctx):
-        sort_byvalue=sorted(self.playerdata.items(),key=lambda x:x[1],reverse=True)
-        for i in sort_byvalue:
-            await ctx.send((i[0],i[1]))
+        
+    def printLeaderboard(self):
+        sortplayers=sorted(self.players,key=lambda x:x.balance,reverse=True)
+        for i in sortplayers:
+            print(i.id,i.balance)
 
     def addplayer(self,name):
         self.players.append(name)
-        self.playerdata[name]=3000
 
-    def updateplayer(self,name,number):
-        self.playerdata[name]+=number
-
+    def updateplayer(self,player,number):
+        for i in self.players:
+            if i.id==player:
+                i.setBalance(number)
+      
