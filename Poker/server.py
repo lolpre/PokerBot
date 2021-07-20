5c4b08d6f1db4cc4d73f9f02f3491927cc17a18e
Catherine Chu <67795784+chuc4@users.noreply.github.com>
2021-07-20 14:27:55 -0400

    Update server.py

--- Poker/server.py
+++ Poker/server.py
@@ -1,9 +1,13 @@
 from Poker.player import Player
+from Poker.announcer import Announcer
+from Poker.pokerwrapper import PokerWrapper
 
 class Server:
     def __init__(self):
         self.players = {}
         self.resets = 0
+        self.announcer = Announcer()
+        self.pokerWrapper = PokerWrapper()
     
     def addPlayer(self, ctx):
         player = Player(ctx.author.name, 3000)
@@ -29,4 +33,32 @@ class Server:
 .top - Check the leaderboards to see who is on top\n\
 .join - Join an already existing Poker game\n\
 (Mods Only) .reset - Reset the balances of everyone in the server```")
+        
+    def nextTurns(self, pool, message): 
+        while len(pool) != 0:
+            hasRaised = False
+            i = 0
+            for i in range(len(pool)):
+                self.announcer.askMove(pool[i].getHand(), hasRaised)
+                format_msg = message.content.lower().strip().split()
+                player.setAction(format_msg)
+                if format_msg == "raise":
+                    self.announcer.reportRaise(pool[i].username, format_msg[1]) 
+                    hasRaised = True
+                    temp = pool.pop(i)
+                    pool.append(temp)
+                if format_msg == "call": 
+                    self.announcer.reportCall(pool[i].username)
+                    i += 1
+                if format_msg == "check":
+                    self.announcer.reportCheck(pool[i].username)
+                    i += 1
+                if format_msg == "fold":
+                    self.announcer.reportFold(pool[i].username)
+                    self.pokerWrapper.removePlayer(message.author.id)
+                    pool.pop(i)
+                
+                
+                
+                
 
