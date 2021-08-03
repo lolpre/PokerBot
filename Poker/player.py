004f9deccb7b170436d3ed8844a59d626aa889c2
trollface11 <85368850+trollface11@users.noreply.github.com>
2021-08-03 16:20:34 -0400

    Update player.py

--- Poker/player.py
+++ Poker/player.py
@@ -1,11 +1,12 @@
+#represents the User and stores infor related to it
 class Player:
 	def __init__(self, id, balance):
 		self.id = id
 		self.balance = balance
-		self.inGame = False
+		self.inGame = False #checks if the user is in a game
 
 	def getBalance(self):
 		return self.balance
 	
 	def setBalance(self, value):
-		self.balance = value
+		self.balance = value #used to update player balance after games
