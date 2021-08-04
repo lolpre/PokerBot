f0442f86c8b696e7336b1c43f40cef7db4b20530
Catherine Chu <67795784+chuc4@users.noreply.github.com>
2021-08-03 21:02:16 -0400

    Update card.py

--- Poker/card.py
+++ Poker/card.py
@@ -1,12 +1,28 @@
+##############################
+# CARD                       #
+# The Card class stores all  #
+# the information regarding  #
+# the poker card, including  #
+# the suit and number of the #
+# card.                      #
+##############################
 class Card:
+	# suit: the suit of the card
+	# val: the number on the card 
+	# emote: the graphic depiction of the card 
 	def __init__(self, suit, val, emote):
 		self.suit = suit
 		self.val = val
 		self.emote = emote
-
+	
+	# returns the image of the card 
 	def show(self):
 		return self.emote
 
+	# gets the number value of the card
+	# this is for cards above the value of 10
+	# and for the ace
+	# (Jack, Queen, King, Ace) 
 	def getNumVal(self):
 		if self.val=="A":
 			return 0
@@ -17,7 +33,10 @@ class Card:
 		if self.val=="J":
 			return 10
 		return int(self.val)-1		
-
+	
+	# gets the suit of the card 
+	# returns a number value, one number 
+	# representing a suit 
 	def getNumSuit(self):
 		if self.suit=="Diamonds":
 			return 0
