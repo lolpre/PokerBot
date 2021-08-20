c50304c9fd8362a12c623635883ad3479be19c7a
carrir2 <72038264+carrir2@users.noreply.github.com>
2021-08-19 20:13:38 -0400

    Update evalhand.py

--- Poker/evalhand.py
+++ Poker/evalhand.py
@@ -1,17 +1,19 @@
 from Poker.card import Card
 from Poker.pokerplayer import PokerPlayer
 
-'''
 
-This is the EvaluateHand class. This class's sole purpose is to
-evaluate the given hand. It runs an algorithm that checks to see
-what is the best combination for the hand. Such combinations are
-a Royal Flush, Two Pair, Straight Flush, Four of a Kind, etc.
-
-'''
 
 class EvaluateHand:
-    
+    '''
+    EvaluateHand
+
+    This is the EvaluateHand class. This class's sole purpose is to
+    evaluate the given hand. It runs an algorithm that checks to see
+    what is the best combination for the hand. Such combinations are
+    a Royal Flush, Two Pair, Straight Flush, Four of a Kind, etc.
+
+    '''
+
     def __init__(self, hand):
         """Constructor to Evaluate Hand. Sets hand to all_cards List."""
         self.all_cards= hand
