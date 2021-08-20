53e456258f001609bdf3a8c46d1a7bdafdde93c9
trollface11 <85368850+trollface11@users.noreply.github.com>
2021-08-19 20:04:25 -0400

    Update player.py

--- Poker/player.py
+++ Poker/player.py
@@ -7,17 +7,22 @@ user uses the ".create" command, regitering an account.
 '''
 class Player:
 
-	# This is the constructor for the Player class. It initializes the id,
-	# balance, and the boolean that states whether the user is in a game or not.
+	
 	def __init__(self, id, balance):
+		"""
+		This is the constructor for the Player class. It initializes the id,
+		balance, and the boolean that states whether the user is in a game or not.
+		"""
 		self.id = id  # unique ID to identify the user
 		self.balance = balance  # current balance of the player
 		self.inGame = False   # check if player is in game
 
-	# This method returns the current balance of the player.
+	
 	def get_balance(self):
+		"""This method returns the current balance of the player."""
 		return self.balance
 	
-	# This method sets the balance of the player to a specific value.
+	
 	def set_balance(self, value):
+		"""This method sets the balance of the player to a specific value."""
 		self.balance = value  # updates player balance after each game
